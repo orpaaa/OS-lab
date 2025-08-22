42986c711076bb9f687be67573a43e87f71efc8f
orpaaa <lamiat.orpa@gmail.com>
2025-08-23 00:24:07 +0600

    Update dsfcfs.py

--- dsfcfs.py
+++ dsfcfs.py
@@ -1,19 +1,26 @@
-def dsfcfs(requests, start_head):
-    head = start_head
-    total_movement = 0
+def fcfs_disk(request, head):
+    total_seek = 0
+    current = head
+    print("seek seq:", end="")
+    for i in requests:
+        if i==head:
+          print(f"{i}(skipped)", end="")
+          head= None
+          continue
+        distance = abs(current - i)
+        total_seek += distance
+        current = i
+        print(i, end=" ")
 
-   
-    filtered_requests = [r for r in requests if r != start_head]
+    print("\ntotal number of seek operations =", total_seek)
 
-    for req in filtered_requests:
-        total_movement += abs(req - head)
-        head = req
+n=(int(input("enter num of reqs:")))
 
-    print("Total Seek Operations:", total_movement)
+requests = []
+print("enter requests:")
+for i in range(n):
+  req=int(input(f"requests{i+1}:"))
+  requests.append(req)
+head = int(input("enter the head position:"))
 
-
-
-requests = [176, 39, 114, 90, 26]
-start_head = 39
-
-dsfcfs(requests, start_head)
+fcfs_disk(requests, head)

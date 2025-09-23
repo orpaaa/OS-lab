f558575da4bcf262946552b23f00a2bdaa2be66a
orpaaa <lamiat.orpa@gmail.com>
2025-09-23 12:10:33 +0600

    Create midq2.py

---
+++ midq2.py
@@ -0,0 +1,33 @@
+def sstf(requests, head):
+    n = len(requests)
+    visited = [False] * n
+    total_seek = 0
+    sequence = [head]
+
+    for _ in range(n):
+        idx = -1
+        min_dist = float('inf')
+
+        for i in range(n):
+            if not visited[i]:
+                dist = abs(head - requests[i])
+                if dist < min_dist:
+                    min_dist = dist
+                    idx = i
+
+        visited[idx] = True
+        total_seek += abs(head - requests[idx])
+        head = requests[idx]
+        sequence.append(head)
+
+    return sequence, total_seek
+
+
+
+requests = [98, 183, 37, 122, 14, 124, 65, 67]
+head = 53
+
+sequence, total_seek = sstf(requests, head)
+
+print("Seek Sequence:", " -> ".join(map(str, sequence)))
+print("Total Seek Operations =", total_seek)
